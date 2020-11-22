2e75bb84fa306ad38efa5d3a3a853a9efbca1385
Alexander Totsky <43824249+Velly69@users.noreply.github.com>
2020-11-22 21:05:49 +0200

    reentrantLock implemented

---
+++ Tasks and Labs/Tasks/eighth-task/src/main/java/reentrant/lock/CustomReentrantLock.java
@@ -0,0 +1,51 @@
+package reentrant.lock;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CustomReentrantLock implements CustomLock{
+    private static final Logger log = Logger.getLogger(CustomReentrantLock.class.getName());
+    private int lockHoldCount;
+    private long currentHoldingThreadID;
+
+    public CustomReentrantLock() {
+        this.lockHoldCount = 0;
+    }
+
+    public synchronized void lock() {
+        if(lockHoldCount == 0){
+            lockHoldCount++;
+            currentHoldingThreadID = Thread.currentThread().getId();
+        } else if(currentHoldingThreadID != Thread.currentThread().getId()){
+            while (currentHoldingThreadID!= Thread.currentThread().getId()){
+                try {
+                    this.wait();
+                    lockHoldCount++;
+                    currentHoldingThreadID = Thread.currentThread().getId();
+                } catch (InterruptedException e) {
+                    log.log(Level.SEVERE, "Exception: ", e);
+                }
+            }
+        } else {
+            lockHoldCount++;
+        }
+    }
+
+    public synchronized void unlock() {
+        if(lockHoldCount == 0){
+            throw new IllegalMonitorStateException();
+        }
+        lockHoldCount--;
+        if(lockHoldCount == 0){
+            notify();
+        }
+    }
+
+    public boolean tryLock() {
+        if(lockHoldCount == 0){
+            lock();
+            return true;
+        }
+        return false;
+    }
+}
